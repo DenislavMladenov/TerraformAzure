806941aff0ad706a6bed7f910a89144fbb011e2e
DenislavMladenov <47724090+DenislavMladenov@users.noreply.github.com>
2025-04-08 21:24:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,36 +11,27 @@ provider "azurerm" {
   features {}
 }
 
-# Generate a random integer to create a globally unique name
-#resource "random_integer" "ri" {
-#  min = 10000
-#  max = 99999
-#}
-
-#Create the resource group
+# Create the resource group
 resource "azurerm_resource_group" "rg" {
- name     = var.resource_group_name
- location = var.resource_group_location
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
-
-
-#Create the web app stack service plan
+# Create the App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = var.app_service_plan_name
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name          
+  location            = azurerm_resource_group.rg.location      
   os_type             = "Linux"
   sku_name            = "F1"
 }
 
-#Create the web app
+# Create the Web App
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
-  location            = var.resource_group_location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location      
+  resource_group_name = azurerm_resource_group.rg.name          
   service_plan_id     = azurerm_service_plan.appserviceplan.id
-
 
   site_config {
     application_stack {
@@ -48,24 +39,25 @@ resource "azurerm_linux_web_app" "app" {
     }
     always_on = false
   }
+
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
     value = "Data Source=tcp:${azurerm_mssql_server.azmssql.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.azmssqldb.name};User ID=${azurerm_mssql_server.azmssql.administrator_login};Password=${azurerm_mssql_server.azmssql.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
   }
-
 }
+
 # Create the SQL Server
 resource "azurerm_mssql_server" "azmssql" {
   name                         = var.sql_server_name
-  resource_group_name          = var.resource_group_name
-  location                     = var.resource_group_location
+  resource_group_name          = azurerm_resource_group.rg.name          
+  location                     = azurerm_resource_group.rg.location      
   version                      = "12.0"
   administrator_login          = var.sql_admin_login
   administrator_login_password = var.sql_admin_password
 }
 
-#Create the SQL Database
+# Create the SQL Database
 resource "azurerm_mssql_database" "azmssqldb" {
   name         = var.sql_database_name
   server_id    = azurerm_mssql_server.azmssql.id
@@ -74,7 +66,7 @@ resource "azurerm_mssql_database" "azmssqldb" {
   sku_name     = "S0"
 }
 
-#Create the SQL Firewall Rule
+# Create the SQL Firewall Rule
 resource "azurerm_mssql_firewall_rule" "azmssqlfirewallrule" {
   name             = var.firewall_rule_name
   server_id        = azurerm_mssql_server.azmssql.id
@@ -82,10 +74,10 @@ resource "azurerm_mssql_firewall_rule" "azmssqlfirewallrule" {
   end_ip_address   = "0.0.0.0"
 }
 
-#Deploy the code from a public GitHub repo
+# Deploy the code from a public GitHub repo
 resource "azurerm_app_service_source_control" "azlwa" {
-  app_id                 = azurerm_linux_web_app.app.id
-  repo_url               = var.repo_URL
-  branch                 = "main"
-  use_manual_integration = true
+  app_id                  = azurerm_linux_web_app.app.id
+  repo_url                = var.repo_URL
+  branch                  = "main"
+  use_manual_integration  = true
 }
