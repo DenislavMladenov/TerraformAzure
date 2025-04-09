322416abb47aaf525ff53fe6342d7fa4e97e9daa
dmladenov <d.mladenov@discoverer.bg>
2025-04-09 11:21:28 +0300

    add backend block to store state file in AZ storage account

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.25.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "tasboardstorage"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
@@ -15,7 +22,9 @@ provider "azurerm" {
 #resource "random_integer" "ri" {
 #  min = 10000
 #  max = 99999
-#} 
+#
+
+
 
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
@@ -26,8 +35,8 @@ resource "azurerm_resource_group" "rg" {
 # Create the App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = var.app_service_plan_name
-  resource_group_name = azurerm_resource_group.rg.name          
-  location            = azurerm_resource_group.rg.location      
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "F1"
 }
@@ -35,8 +44,8 @@ resource "azurerm_service_plan" "appserviceplan" {
 # Create the Web App
 resource "azurerm_linux_web_app" "app" {
   name                = var.app_service_name
-  location            = azurerm_resource_group.rg.location      
-  resource_group_name = azurerm_resource_group.rg.name          
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.appserviceplan.id
 
   site_config {
@@ -56,8 +65,8 @@ resource "azurerm_linux_web_app" "app" {
 # Create the SQL Server
 resource "azurerm_mssql_server" "azmssql" {
   name                         = var.sql_server_name
-  resource_group_name          = azurerm_resource_group.rg.name          
-  location                     = azurerm_resource_group.rg.location      
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = var.sql_admin_login
   administrator_login_password = var.sql_admin_password
@@ -82,8 +91,8 @@ resource "azurerm_mssql_firewall_rule" "azmssqlfirewallrule" {
 
 # Deploy the code from a public GitHub repo
 resource "azurerm_app_service_source_control" "azlwa" {
-  app_id                  = azurerm_linux_web_app.app.id
-  repo_url                = var.repo_URL
-  branch                  = "main"
-  use_manual_integration  = true
+  app_id                 = azurerm_linux_web_app.app.id
+  repo_url               = var.repo_URL
+  branch                 = "main"
+  use_manual_integration = true
 }
--- values.tfvars
+++ values.tfvars
@@ -1,13 +1,13 @@
-resource_group_name = "TaskBoardRG"
+resource_group_name     = "TaskBoardRG"
 resource_group_location = "North Europe"
-app_service_plan_name = "web-app-service-plan"
-app_service_name = "denislav-webapp"
-sql_server_name = "denislav-taskboard12"
-sql_database_name = "denislav_sqldatabase"
-sql_admin_login = "sqladmin"
-sql_admin_password = "sqlpassword123@"
-firewall_rule_name = "sqlfirewallrule"
-repo_URL = "https://github.com/DenislavMladenov/TaskBoard"
+app_service_plan_name   = "web-app-service-plan"
+app_service_name        = "denislav-webapp"
+sql_server_name         = "denislav-taskboard12"
+sql_database_name       = "denislav_sqldatabase"
+sql_admin_login         = "sqladmin"
+sql_admin_password      = "sqlpassword123@"
+firewall_rule_name      = "sqlfirewallrule"
+repo_URL                = "https://github.com/DenislavMladenov/TaskBoard"
 
 
 
